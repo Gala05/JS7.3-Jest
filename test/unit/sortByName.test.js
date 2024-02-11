5793221257f5e024bc670e435d822bab50404316
Galina Mikhaylova <gala6858@mail.ru>
2024-02-11 17:46:22 +0300

    решение к дз Jest

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,43 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",      
+    ]
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",      
+    ]
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);    
+  });
+
+  test ("Without params throws exception", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError)
+  });
+
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Унесенные ветром",
+      "Горе от ума",
+      "Горе от ума",          
+    ]
+
+    const expected = [
+      "Горе от ума",
+      "Горе от ума",
+      "Унесенные ветром",     
+    ]
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);    
+  });
+});
